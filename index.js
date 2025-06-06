de91d779900d79e9741c55ce6116f1a07d0f86f9
Navis Ochieng <navis.ochieng@student.moringaschool.com>
2025-06-06 08:32:17 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceInFeet){
+  if (distanceInFeet <= 400) {
+    return "This one is on me!"
+  } else if (distanceInFeet <= 2000) {
+    return "That will be twenty bucks."
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === "NYC" || city === "New York City") {
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+  if (tipAmount === "generous") {
+    return "Thank you so much."
+  } else if (tipAmount === "not as generous") {
+    return "Thank you."
+  } else {
+    return "Bye."
+  }
 }
